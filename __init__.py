630baf9e882d0780f0f463be1d04051ba7e80c66
aspli <alexandre.xoquito@gmail.com>
2026-03-25 12:41:54 -0300

    up files

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .leilaoimovel import buscar_leilao_imovel_html
+from .sold import buscar_leiloes_sold_api
+from .zuk import buscar_portal_zuk
+
+# You could also define package-level variables here:
+__version__ = "1.0.0"
